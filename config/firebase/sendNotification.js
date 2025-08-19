86f24b8138f2a1f4bdba6fb76679754274b01375
Nishant Sharma <96777814+nishantsharma13903@users.noreply.github.com>
2025-08-19 10:40:52 +0530

    updaload

--- config/firebase/sendNotification.js
+++ config/firebase/sendNotification.js
@@ -1,61 +1,61 @@
-const admin = require('firebase-admin');
-const serviceAccount = require('./test.json');
+// const admin = require('firebase-admin');
+// const serviceAccount = require('./test.json');
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+// });
 
-exports.sendNotificationToUser = async (deviceToken, title, body,imageUrl, data,actionPage) => {
-  const message = {
-    token: deviceToken,
-    notification: {
-      title: title,
-      body: body,
-      image: imageUrl,  // URL or path to the image file
-    },
-    android: {
-      notification: {
-        channel_id: 'nexkaarya', // Specify your channel ID here
-      }
-    },
-    apns: {
-      payload: {
-        aps: {
-          alert: {
-            title: title,
-            body: body,
-          },
-          sound: 'default',
-          badge: 1,  // Optional: Badge count on the app icon
-          'mutable-content': 1  // Required for rich notifications (images, etc.)
-        },
-      },
-      fcm_options: {
-        image: imageUrl  // URL or path to the image file
-      }
-    },
-    data: {
-      ...data,
-      click_action: actionPage  // Custom action
-    },
-  };
+// exports.sendNotificationToUser = async (deviceToken, title, body,imageUrl, data,actionPage) => {
+//   const message = {
+//     token: deviceToken,
+//     notification: {
+//       title: title,
+//       body: body,
+//       image: imageUrl,  // URL or path to the image file
+//     },
+//     android: {
+//       notification: {
+//         channel_id: 'indfashion', // Specify your channel ID here
+//       }
+//     },
+//     apns: {
+//       payload: {
+//         aps: {
+//           alert: {
+//             title: title,
+//             body: body,
+//           },
+//           sound: 'default',
+//           badge: 1,  // Optional: Badge count on the app icon
+//           'mutable-content': 1  // Required for rich notifications (images, etc.)
+//         },
+//       },
+//       fcm_options: {
+//         image: imageUrl  // URL or path to the image file
+//       }
+//     },
+//     data: {
+//       ...data,
+//       click_action: actionPage  // Custom action
+//     },
+//   };
 
-  console.log("Message", message);
+//   console.log("Message", message);
 
-  try {
-    const response = await admin.messaging().send(message);
-    console.log('Successfully sent message:', response);
-    return {
-      success : 1,
-      result : response,
-      payload : message
-    }
-  } catch (error) {
-    console.error('Error sending message:', error);
-    return {
-      success : 0,
-      result : error,
-      payload : message
-    };
-  }
-};
+//   try {
+//     const response = await admin.messaging().send(message);
+//     console.log('Successfully sent message:', response);
+//     return {
+//       success : 1,
+//       result : response,
+//       payload : message
+//     }
+//   } catch (error) {
+//     console.error('Error sending message:', error);
+//     return {
+//       success : 0,
+//       result : error,
+//       payload : message
+//     };
+//   }
+// };
