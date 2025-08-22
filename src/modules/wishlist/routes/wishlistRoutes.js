51d0baca1ea9772538887fbae87226230d7c11cb
Nishant Sharma <96777814+nishantsharma13903@users.noreply.github.com>
2025-08-22 10:47:17 +0530

    upload.none()

--- src/modules/wishlist/routes/wishlistRoutes.js
+++ src/modules/wishlist/routes/wishlistRoutes.js
@@ -3,8 +3,8 @@ const wishlistRouter = express.Router();
 const wishlistController = require("../controller/wishlistController");
 const verifyToken = require("../../../middlewares/verifyJWT");
 
-wishlistRouter.post("/add", verifyToken.decodeToken, wishlistController.addToWishlist);
-wishlistRouter.post("/remove", verifyToken.decodeToken, wishlistController.removeFromWishlist);
-wishlistRouter.get("/get-all", verifyToken.decodeToken, wishlistController.getWishlist);
+wishlistRouter.post("/add", verifyToken.decodeToken,upload.none(), wishlistController.addToWishlist);
+wishlistRouter.post("/remove", verifyToken.decodeToken,upload.none(), wishlistController.removeFromWishlist);
+wishlistRouter.get("/get-all", verifyToken.decodeToken,upload.none(), wishlistController.getWishlist);
 
 module.exports = wishlistRouter;
