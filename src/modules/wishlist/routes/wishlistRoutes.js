6e1759a26963dd357185c723bae9e2b091b932de
Nishant Sharma <96777814+nishantsharma13903@users.noreply.github.com>
2025-08-22 10:47:41 +0530

    http://172.25.48.1:5010/api

--- src/modules/wishlist/routes/wishlistRoutes.js
+++ src/modules/wishlist/routes/wishlistRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const wishlistRouter = express.Router();
 const wishlistController = require("../controller/wishlistController");
 const verifyToken = require("../../../middlewares/verifyJWT");
+const upload = require('../../../middlewares/multer')
 
 wishlistRouter.post("/add", verifyToken.decodeToken,upload.none(), wishlistController.addToWishlist);
 wishlistRouter.post("/remove", verifyToken.decodeToken,upload.none(), wishlistController.removeFromWishlist);
